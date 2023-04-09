53bed109472837934f15fa3d10916384637f3ad7
Aleksandr Kharitonovich <harison-din@yandex.ru>
2023-04-09 16:07:49 +0300

    Реализовано оба задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
+  let d = Math.pow(b,2)-4*a*c;
+
+  if (d < 0) {
+    return [];
+  }
+
+  if (d === 0) {
+    console.log(-b/(2*a));
+    return [-b/(2*a)];
+  }
+
   let arr = [];
-  
+
+  arr.push((-b + Math.sqrt(d) )/(2*a));
+  arr.push((-b - Math.sqrt(d) )/(2*a));
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)){
+    return false;
+  }
+
+  percent = percent/100/12;
+
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+
+  totalPayment = Number(totalPayment.toFixed(2));
+
+  return totalPayment;
 }
